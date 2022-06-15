74b8dc75c3235989c3ac4d3df7dba6e0761d477d
katiaconchas <knconchasanchez@gmail.com>
2022-06-15 18:00:52 -0400

    add usu bbdd

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
-
+import email
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import Usuario
 # Create your views here.
 def signIn(request):
     return render(request, 'signIn.html')
 
+def validarUsuario(request):
+    #Recibimos los datos desde el formulario que fueron
+    #pasados vias POST
+    v_email=request.POST.get('email')
+    v_password=request.POST.get('password')
+
+    try:
+    #Buscamos el usuario en la base de datos
+        usu=Usuario.objects.get(email=v_email, password=v_password)
+
+        if usu:
+            request.session['usuario'] = v_email
+            return redirect('/PetsFriends_usuario/')
+    except:
+        return redirect('/signIn/')
+
+
+def PetsFriends_usuario(request):
+    if 'usuario' not in request.session:
+        return redirect('/signIn/')
+    return render(request, 'Pets_Friends_usuario.html')
